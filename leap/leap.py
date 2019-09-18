8e515f9684ca38ac97cb656ca4da16c54a835591
Carol Seregni <cseregni@chicagobooth.edu>
2019-09-18 19:28:33 +0000

    123

--- leap/leap.py
+++ leap/leap.py
@@ -1,8 +1,2 @@
 def leap_year(year):
-   return (x % 400==0 
-
-
-
-    on every year that is evenly divisible by 4
-  except every year that is evenly divisible by 100
-    unless the year is also evenly divisible by 400
+   pass
