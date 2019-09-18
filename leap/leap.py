f0adfa55824e913482be2e92b12edea180a11094
Jiahao Chen <cjiaohao61@gmail.com>
2019-09-18 15:22:03 +0000

    finish leap

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,13 @@
+#on every year that is evenly divisible by 4
+#  except every year that is evenly divisible by 100
+#    unless the year is also evenly divisible by 400
+
 def leap_year(year):
-    pass
+    div_4 = year % 4 == 0
+    div_100 = year % 100 == 0
+    div_400 = year % 400 ==0
+    leap = ((div_4) & (not div_100)) | ((div_4) & (div_100) & (div_400)) 
+    
+    return leap
+
+print(leap_year(2000))
