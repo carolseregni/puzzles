7c11f6eddfe50a1f57c8ed50214b9c7d6277d8b7
Julien Weber <julien.weber@chicagobooth.edu>
2019-09-18 16:09:56 +0000

    change

--- change/change.py
+++ change/change.py
@@ -1,2 +1,13 @@
 def find_fewest_coins(coins, target):
-    pass
+    
+    numberofcoins=len(coins)
+    coins=sorted(coins, reverse=True)
+    target_remaining=target
+    n=[]
+    for i in range(0,numberofcoins, 1):
+        n = [n + [target_remaining//coins[i]]]
+        target_remaining=target_remaining%coins[i]
+
+    return(n)
+
+print(find_fewest_coins([5, 10, 20, 2], 57))
