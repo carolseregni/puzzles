b2cf40f806a1c2f8fbe4f0316e8e812c45314b7a
Carol Seregni <cseregni@chicagobooth.edu>
2019-09-18 16:40:25 +0000

    acronym done

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,2 +1,15 @@
 def abbreviate(words):
-    
+    letters = ""
+    words = words.split()
+    for word in words:
+        letters = letters + word[0]
+    output =""+(letters).upper()
+    return(output)
+
+print(abbreviate("Ciao bella"))
+
+
+
+
+
+
